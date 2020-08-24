e97f8c6f7b93ff546c4f22ffe2b54b50c1fb89c9
salpoz <salaam001@hotmail.com>
2020-08-24 15:09:58 +0500

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,16 @@ weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const location = search.value;
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        console.log(data);
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-        } else {
-          messageOne.textContent = "Temperature: " + data.temperature;
-          messageTwo.textContent = "Country: " + data.country;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = "Temperature: " + data.temperature;
+        messageTwo.textContent = "Country: " + data.country;
+      }
+    });
+  });
 });
